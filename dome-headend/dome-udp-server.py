e9bd06fb1027a9fae21a0f6676c65185aa17bf34
Thomas Lee <thomasslee97@gmail.com>
2018-08-27 21:19:12 +0100

    Added api sender

--- dome-headend/dome-udp-server.py
+++ dome-headend/dome-udp-server.py
@@ -11,6 +11,9 @@ from DomeConfig import DomeConfig
 DOMEJS_HOST = "localhost"
 DOMEJS_PORT = 1444
 
+DOMEAPI_HOST = "localhost"
+DOMEAPI_PORT = 7778
+
 CONTROLLER_PORT = 10460
 
 ADDRESSABLE_LED_SERVER_HOST = 'localhost'
@@ -50,10 +53,11 @@ FORMAT_CONS = '%(asctime)s %(name)-12s %(levelname)8s\t%(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT_CONS)
 
 class DomeHeadendConfig(DomeConfig):
-	def __init__(self, config, domejs_host, domejs_port, addressable_led_server_host, addressable_led_server_port, controller_port, controllerIDsToIps):
+	def __init__(self, config, domejs_host, domejs_port, addressable_led_server_host, addressable_led_server_port, controller_port, api_host, api_port, controllerIDsToIps):
 		super(DomeHeadendConfig, self).__init__(config, controllerIDsToIps)
 		self.domejsSender = BigPacketSender(domejs_host, domejs_port)
 		self.addressableLEDSender = BigPacketSender(addressable_led_server_host, addressable_led_server_port)
+		self.apiSender = BigPacketSender(api_host, api_port)
 		self.controllerPort = controller_port
 
 	def process_command(self, command):
@@ -68,6 +72,7 @@ class DomeHeadendConfig(DomeConfig):
 
 			self.domejsSender.send(command, socket.AF_INET)
 			self.addressableLEDSender.send(bytearray("$") + command, socket.AF_INET6)
+			self.apiSender.send(command, socket.AF_INET)
 		return 0
 
 def udp_server(host='localhost', port=3663):
@@ -85,7 +90,7 @@ def main():
 
 	with open('dome.config') as config_file:
 		data = json.load(config_file)
-		config = DomeHeadendConfig(data, DOMEJS_HOST, DOMEJS_PORT, ADDRESSABLE_LED_SERVER_HOST, ADDRESSABLE_LED_SERVER_PORT, CONTROLLER_PORT, CONTTROLLER_IDS_TO_IPS)
+		config = DomeHeadendConfig(data, DOMEJS_HOST, DOMEJS_PORT, ADDRESSABLE_LED_SERVER_HOST, ADDRESSABLE_LED_SERVER_PORT, CONTROLLER_PORT, DOMEAPI_HOST, DOMEAPI_PORT, CONTTROLLER_IDS_TO_IPS)
 
 	for data in udp_server():
 		#config.process_command(data[0], data[1][0])
