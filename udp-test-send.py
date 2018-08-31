d219563acccb91b8f350e043665bba463e1eb495
Sam Nicholson <sam.nicholson@bbc.co.uk>
2018-08-31 18:20:31 +0100

    Tidy up, make the linter happy

--- udp-test-send.py
+++ udp-test-send.py
@@ -9,20 +9,23 @@ OFF = bytearray(0 for i in range(MAX_LED*3))
 ON = bytearray(255 for i in range(MAX_LED*3))
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def chase(r=255, g=255, b=255, sleep = 0.01):
-	for i in range(0, MAX_LED):
-		output = bytearray(0 for i in range(MAX_LED*3))
-		col = (r << 16) + (g << 8) + (b)
-		output[i*3:(i*3)+6] = col.to_bytes(6,byteorder='big')
-		sock.sendto(output, (UDP_IP, UDP_PORT))
-		time.sleep(sleep)
-	sock.sendto(OFF, (UDP_IP, UDP_PORT))
-	
-def rainbow(sleep = 0.2):
-	pass
+
+def chase(r=255, g=255, b=255, sleep=0.01):
+    for i in range(0, MAX_LED):
+        output = bytearray(0 for i in range(MAX_LED*3))
+        col = (r << 16) + (g << 8) + (b)
+        output[i*3:(i*3)+6] = col.to_bytes(6, byteorder='big')
+        sock.sendto(output, (UDP_IP, UDP_PORT))
+        time.sleep(sleep)
+    sock.sendto(OFF, (UDP_IP, UDP_PORT))
+
+
+def rainbow(sleep=0.2):
+    pass
+
 
 while True:
-	sock.sendto(ON, (UDP_IP, UDP_PORT))
-	time.sleep(1)
-	sock.sendto(OFF, (UDP_IP, UDP_PORT))
-	time.sleep(1)
+    sock.sendto(ON, (UDP_IP, UDP_PORT))
+    time.sleep(1)
+    sock.sendto(OFF, (UDP_IP, UDP_PORT))
+    time.sleep(1)
